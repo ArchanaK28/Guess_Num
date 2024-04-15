43beea31fdd6c4d49f9af75e4600c71636d80832
ArchanaK28 <78334508+ArchanaK28@users.noreply.github.com>
2024-04-15 14:22:41 +0530

    Add files via upload

---
+++ Result.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Result ({secretNum, term}){
+    let result;
+
+    if(term){
+        if(term > secretNum){
+            result = 'Higher';
+        }else if(term < secretNum){
+            result = 'lower';
+        }else if(term == secretNum){
+            result = "Correct";
+        }else{
+            result = "Please enter a valid number";
+        }}
+   
+    return <h3>You Guessed: {result}</h3>
+} 
+export default Result;
